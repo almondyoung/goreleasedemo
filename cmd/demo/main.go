0f92721f642f5739f73e7a25ee914399f907a173
yt <276531137@qq.com>
2023-12-18 13:42:37 +0800

    feat: add demo

---
+++ cmd/demo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "myapp",
+		Short: "A simple command-line application",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello, World!")
+		},
+	}
+
+	greetCmd := &cobra.Command{
+		Use:   "greet",
+		Short: "Greet someone",
+		Run: func(cmd *cobra.Command, args []string) {
+			name, _ := cmd.Flags().GetString("name")
+			fmt.Printf("Hello, %s!\n", name)
+		},
+	}
+
+	greetCmd.Flags().StringP("name", "n", "", "Name of the person to greet")
+
+	rootCmd.AddCommand(greetCmd)
+
+	rootCmd.Execute()
+}
